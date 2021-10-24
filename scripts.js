b5b7bb9f8f0f8adaeb17a24b0c9f847d49f60037
Keemo <Keemoyen4@gmail.com>
2021-10-23 20:18:10 -0700

    index.html

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
+"use strict";
 /* Add your JavaScript to this file */
+console.log("This script is connected");
+
+var body = document.getElementsByTagName("div")[3];
+var section = document.getElementsByClassName("container");
+console.log(body);
+
+
+/*document.getElementsByClassName("btn").addEventListener("click",)
+*/
